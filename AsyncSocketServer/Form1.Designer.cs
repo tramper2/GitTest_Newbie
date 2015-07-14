e78b24eaa29c6f9ec8f08e6fa54a5814980f6bcd
Danny Woo <tramper2@daum.net>
2015-07-14 12:48:53 +0900

    파일추가

---
+++ AsyncSocketServer/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿namespace AsyncSocketServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        //protected override void Dispose(bool disposing)
+        //{
+        //    if (disposing && (components != null))
+        //    {
+        //        components.Dispose();
+        //    }
+        //    base.Dispose(disposing);
+        //}
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 24);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(188, 226);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "수신메시지";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(210, 262);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "AsynServer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ AsyncSocketServer/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+
+namespace AsyncSocketServer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Socket m_ServerSocket;
+        private List<Socket> m_ClientSocket;
+        private byte[] szData;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            m_ClientSocket = new List<Socket>();
+
+            m_ServerSocket = new Socket(
+                                AddressFamily.InterNetwork, 
+                                SocketType.Stream, 
+                                ProtocolType.Tcp);
+            IPEndPoint ipep = new IPEndPoint(IPAddress.Any, 10000);
+            m_ServerSocket.Bind(ipep);  
+            m_ServerSocket.Listen(20);  
+
+            SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+            args.Completed 
+                += new EventHandler<SocketAsyncEventArgs>(Accept_Completed);
+            m_ServerSocket.AcceptAsync(args);
+        }
+
+        private void Accept_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            Socket ClientSocket = e.AcceptSocket;
+            m_ClientSocket.Add(ClientSocket);
+
+            if (m_ClientSocket != null)
+            {
+                SocketAsyncEventArgs args = new SocketAsyncEventArgs();
+                szData = new byte[1024]; 
+                args.SetBuffer(szData, 0, 1024);
+                args.UserToken = m_ClientSocket;
+                args.Completed 
+                    += new EventHandler<SocketAsyncEventArgs>(Receive_Completed);
+                ClientSocket.ReceiveAsync(args);
+            }
+            e.AcceptSocket = null;
+            m_ServerSocket.AcceptAsync(e);
+        }
+        private void Receive_Completed(object sender, SocketAsyncEventArgs e)
+        {
+            Socket ClientSocket = (Socket)sender;
+            if (ClientSocket.Connected && e.BytesTransferred > 0)
+            {
+                byte[] szData = e.Buffer;    // 데이터 수신
+                string sData = Encoding.Unicode.GetString(szData);
+
+                string Test = sData.Replace("\0", "").Trim();
+                SetText(Test);
+                for (int i = 0; i < szData.Length; i++)
+                {
+                    szData[i] = 0;
+                }
+                e.SetBuffer(szData, 0, 1024);
+                ClientSocket.ReceiveAsync(e);
+            }
+            else
+            {
+                ClientSocket.Disconnect(false);
+                ClientSocket.Dispose();
+                m_ClientSocket.Remove(ClientSocket);
+            }
+        }
+        private delegate void SetTextCallback(string text);
+        private void SetText(string text)
+        {
+            if (richTextBox1.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                if (richTextBox1.TextLength > 0)
+                {
+                    richTextBox1.AppendText("\n");
+                }
+                richTextBox1.AppendText(text);
+                richTextBox1.ScrollToCaret();
+            }
+        }
+        protected override void Dispose(bool disposing)
+        {
+            foreach (Socket pBuffer in m_ClientSocket)
+            {
+                if(pBuffer.Connected)
+                    pBuffer.Disconnect(false);
+                pBuffer.Dispose();
+            }
+            m_ServerSocket.Dispose();
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
